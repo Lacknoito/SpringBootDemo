447afbf7b2c54a78f43952cb0d473910728c2db6
LAPTOP-VN0R6TH7\lacno <lac.noito@gmail.com>
2020-10-18 10:56:11 +0700

    start connect mongodb

---
+++ demo/src/main/java/com/example/demo/mongo/entity/User.java
@@ -0,0 +1,22 @@
+package com.example.demo.mongo.entity;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "user")
+public class User {
+	private String username;
+	private String password;
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ demo/src/main/java/com/example/demo/mongo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.mongo.repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.demo.mongo.entity.User;
+
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
